3814d9f1519dc981c678d17bec0707c8dc0b75d4
K-R-Nagendra-Reddy <nagendrakothurureddivari@gmail.com>
2024-02-25 17:20:13 +0000

    code

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Navigation from './components/Navigation'
+import Header from './components/Header'
+import Home from './components/Home'
+import './App.css'
+
+// Replace your code here
+const App = () => <Home />
+export default App
---
+++ src/components/HomeVideos/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const UlContainer = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  background-color: transparent;
+`
+export const ListOfVideo = styled.list`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 15vh;
+  width: 25vw;
+`
+export const ListBottomContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
